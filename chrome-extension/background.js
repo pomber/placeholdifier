dfb56eefacd721ef08f527708fb3bca37e5cea3d
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-07-01 16:51:45 -0300

    Add chrome extension

---
+++ chrome-extension/background.js
@@ -0,0 +1,22 @@
+function placeholdify() {
+  chrome.tabs.executeScript({
+    code: code,
+  });
+}
+
+chrome.browserAction.onClicked.addListener(placeholdify);
+
+const href = "https://unpkg.com/placeholdifier@0.1.0/placeholdifier.css";
+const linkId = "placeholdifier";
+const code = `
+const id = "${linkId}";
+if (!document.getElementById(id)) {
+  const head = document.getElementsByTagName("head")[0];
+  const link = document.createElement("link");
+  link.id = id;
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = "${href}";
+  link.media = "all";
+  head.appendChild(link);
+}`;
